83dfafe9e53fe2e22e81274d90aec09aa5208d30
JustinChen <yub612@gmail.com>
2024-05-17 01:35:45 +0800

    form ui ver1

--- app/add/page.js
+++ app/add/page.js
@@ -1,34 +1,38 @@
 'use client'
-
-import { isNotFoundError } from "next/dist/client/components/not-found"
+// import { isNotFoundError } from "next/dist/client/components/not-found"
 import { useRouter } from 'next/navigation'
 import { useState } from "react"
 import Form from "../components/form"
 
 export default function Add(){
     return (
-        <>
-        <RoomForm/>
-        <CabinetForm/>
-        <BoxForm/>
-        <ItemForm/>
-        <Msg/>
-        </>
+        <div className="relative my-10 px-6 pt-6 pb-6
+         bg-slate-100 shadow-xl ring-1 ring-gray-900/5
+        sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+            <RoomForm/>
+            <CabinetForm/>
+            <BoxForm/>
+            <ItemForm/>
+            <Msg type="error"/>
+            <Msg type="success"/>
+        </div>
     )
 }
-const Msg = ()=>{
-    return (
-        <>
-        <p>sucess</p>
-        <p>error</p>
-        </>
-    )
+const Msg = ({type})=>{
+    switch(type){
+        case "success":
+            return <p className="m-1 p-2 bg-green-500 border-2 w-full text-lg rounded">sucess</p>
+            break
+        case "error":
+            return <p className="m-1 p-2 bg-red-500 border-2 w-full text-lg rounded">error</p>
+            break
+    }
 }
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 const asyncSend = async (obj) => {
-    console.log("store")
+    console.log("store",obj)
     localStorage.setItem("db", JSON.stringify(obj))
     // await timeout(2000)
     console.log("stored!!!!")
@@ -44,68 +48,112 @@ const changeHaldler = (setItm,itm,func=()=>{})=>(e,type)=>{
     func()
     setItm(nitm)
 }
-
-const CabinetForm = ()=>{
-    const defaultObj = {name:""}
-    const [itm,setItm] = useState({
-        name:"lorem"
-    })
-    const handleSubmit = submitHandler(setItm,defaultObj,
-        async ()=>{await asyncSend(itm)})
-    const handleChange = changeHaldler(setItm,itm)
-    return (
-        <form onSubmit={handleSubmit}>
-            <h3>Cabinet</h3>
-            <label>name</label>
-            <input 
-                value={itm.name} 
-                onChange={(e)=>handleChange(e,"name")}></input>
-            {/* <br/> */}
-            <input type="submit" value="submit" />
-        </form>
-    )
-}
-const BoxForm = ()=>{
-    const defaultObj = {name:""}
-    const router = useRouter()
-    const [itm,setItm] = useState({
-        name:"lorem"
-    })
-    const handleSubmit = submitHandler(setItm,defaultObj,async ()=>{await asyncSend(itm)})
-    const handleChange = changeHaldler(setItm,itm)
-    return (
-        <form onSubmit={handleSubmit}>
-            <h3>Room</h3>
-            <label>name</label>
-            <input 
-                value={itm.name} 
-                onChange={(e)=>handleChange(e,"name")}></input>
-            {/* <br/> */}
-            <select name="" id="">
-                <option value="test">test</option>
-            </select>
-            <input type="submit" value="submit" />
-        </form>
-    )
-}
 const RoomForm = ()=>{
-    const defaultObj = {name:""}
+    const defaultObj = {name:"lorem"}
     const router = useRouter()
-    const [itm,setItm] = useState({
-        name:"lorem"
-    })
+    const [itm,setItm] = useState(defaultObj)
     // const 
     const handleSubmit = submitHandler(setItm,defaultObj,async ()=>{await asyncSend(itm)})
     const handleChange = changeHaldler(setItm,itm)
     return (
         <form onSubmit={handleSubmit}>
-            <h3>Room</h3>
-            <label>name</label>
-            <input 
+            <p className="text-3xl">Room</p>
+            <label className="block text-lg">name</label>
+            <input
+                className="w-full pl-2 outline outline-1 rounded text-lg"
                 value={itm.name} 
                 onChange={(e)=>handleChange(e,"name")}></input>
-            {/* <br/> */}
-            <input type="submit" value="submit" />
+            <label className="block text-lg">image</label>
+            <input className="w-full bg-gray-200 text-lg rounded-lg file:border-0 file:bg-gray-300 file:hover:bg-gray-50" 
+            type="file" name="" id=""/>
+            <div className="flex justify-end ">
+            <input className="mt-2 p-1 rounded bg-cyan-700 hover:bg-cyan-300 text-white" 
+            type="submit" value="submit" />
+            </div>
+        </form>
+    )
+}
+const CabinetForm = ()=>{
+    const defaultObj = {name:"",room:"hello"}
+    const [itm,setItm] = useState(defaultObj)
+    const handleSubmit = submitHandler(setItm,defaultObj,
+        async ()=>{
+            console.log(itm)
+            await asyncSend(itm)
+        })
+    const handleChange = changeHaldler(setItm,itm)
+    return (
+        <form onSubmit={handleSubmit}>
+            <p className="text-3xl">Cabinet</p>
+            <label className="block text-lg">name</label>
+            <input 
+                className="w-full pl-2 outline outline-1 rounded text-lg"
+                value={itm.name} 
+                onChange={(e)=>handleChange(e,"name")}></input>
+            <label className="block text-lg">room</label>
+            <div className="mb-2">
+            <select 
+                className="rounded-lg text-lg w-auto min-w-28 focus:border-1"
+                name="" id="" value={itm.room}
+                onChange={(e)=>handleChange(e,"room")}>
+                <option value="hi">hi</option>
+                <option value="hello">hello</option>
+                {/* <option value="lorem loremlorem loremlorem loremlorem lorem">lorem loremlorem loremlorem loremlorem lorem</option> */}
+            </select>
+            </div>
+            <input className="w-full bg-gray-200 text-lg rounded-lg file:border-0 file:bg-gray-300 file:hover:bg-gray-50" type="file"/>
+            <div className="flex justify-end mt-2">
+                <input className="mt-2 p-1 rounded bg-cyan-700 hover:bg-cyan-300 text-white" 
+                    type="submit" value="submit" />
+            </div>
+        </form>
+    )
+}
+const BoxForm = ()=>{
+    const defaultObj = {
+        name:"",
+        room:"hi",
+        cabinet:"hello"
+    }
+    const router = useRouter()
+    const [itm,setItm] = useState(defaultObj)
+    const handleSubmit = submitHandler(setItm,defaultObj,async ()=>{await asyncSend(itm)})
+    const handleChange = changeHaldler(setItm,itm)
+    return (
+        <form onSubmit={handleSubmit}>
+            <p className="text-3xl">Box</p >
+            <label className="block text-lg ">name</label>
+            <input
+                className="w-full pl-2 outline outline-1 rounded text-lg"
+                value={itm.name} 
+                onChange={(e)=>handleChange(e,"name")}></input>
+            <label className="block text-lg">room</label>
+            <div className="mb-2">
+                <select
+                    className="rounded-lg text-lg w-auto min-w-28 focus:border-1"
+                    name="" id="" value={itm.room} 
+                    onChange={(e)=>handleChange(e,"room")}>
+                    <option value="hi">hi</option>
+                    <option value="hello">hello</option>
+                </select>
+            </div>
+            <label>cabinet</label>
+            <div className="mb-2">
+                <select 
+                    className="rounded-lg text-lg w-auto min-w-28 focus:border-1"
+                    name="" id="" value={itm.cabinet} 
+                    onChange={(e)=>handleChange(e,"cabinet")}>
+                    <option value="hi">hi</option>
+                    <option value="hello">hello</option>
+                </select>
+            </div>
+            <input 
+                className="w-full bg-gray-200 text-lg rounded-lg file:border-0 file:bg-gray-300 file:hover:bg-gray-50" 
+                type="file" name="" id=""/>
+            <div className="flex justify-end mt-2">
+            <input className="mt-2 p-1 rounded bg-cyan-700 hover:bg-cyan-300 text-white" 
+                type="submit" value="submit" />
+            </div>
         </form>
     )
 }
@@ -114,44 +162,67 @@ const ItemForm = ()=>{
         name:"lorem",
         number:1,
         room:"hi",
+        cabinent:"no",
+        box:"hi",
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aut perferendis exercitationem delectus iusto, molestiae quam ratione magni eum necessitatibus laborum, deleniti numquam quibusdam veniam tempora nisi voluptatibus commodi porro."
     }
-    const [itm,setItm] = useState({
-        name:"lorem",
-        number:1,
-        room:"hi",
-        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aut perferendis exercitationem delectus iusto, molestiae quam ratione magni eum necessitatibus laborum, deleniti numquam quibusdam veniam tempora nisi voluptatibus commodi porro."
-        
-    })
+    const [itm,setItm] = useState(defaultObj)
     const handleSubmit = submitHandler(setItm,defaultObj,async ()=>{await asyncSend(itm)})
     const handleChange = changeHaldler(setItm,itm)
     return (
         <form onSubmit={handleSubmit}>
-            <h3>Item</h3>
-            <label>name</label>
-            <input 
+            <p className="text-3xl">Item</p>
+            <label className="block text-lg">name</label>
+            <input
+                className="w-full pl-2 outline outline-1 rounded text-lg"
                 value={itm.name} 
                 onChange={(e)=>handleChange(e,"name")}></input>
-            <br/>
-            <label>number</label>
-            <input 
+            <label className="block text-lg">number</label>
+            <input
+                className="w-full pl-2 outline outline-1 rounded text-lg"
                 type="number" value={itm.number} 
                 onChange={(e)=>handleChange(e,"number")}></input>
-            <br/>
-            <label>room</label>
-            <select 
-                name="" id="" value={itm.room} 
-                onChange={(e)=>handleChange(e,"room")}>
-                <option value="hi">hi</option>
-                <option value="hello">hello</option>
-            </select>
-            <br/>
-            <label>description</label>
-            <textarea 
+            <label className="block text-lg">room</label>
+            <div className="mb-2">
+                <select 
+                    className="rounded-lg text-lg w-auto min-w-28 focus:border-1"
+                    name="" id="" value={itm.room} 
+                    onChange={(e)=>handleChange(e,"room")}>
+                    <option value="hi">hi</option>
+                    <option value="hello">hello</option>
+                </select>
+            </div>
+            <label className="block text-lg">cabinet</label>
+            <div className="mb-2">
+                <select
+                    className="rounded-lg text-lg w-auto min-w-28 focus:border-1"
+                    name="" id="" value={itm.cabinet} 
+                    onChange={(e)=>handleChange(e,"cabinet")}>
+                    <option value="hi">hi</option>
+                    <option value="hello">hello</option>
+                </select>
+            </div>
+            <label className="block text-lg">box</label>
+            <div className="mb-2">
+                <select 
+                    className="rounded-lg text-lg w-auto min-w-28 focus:border-1"
+                    name="" id="" value={itm.box} 
+                    onChange={(e)=>handleChange(e,"box")}>
+                    <option value="hi">hi</option>
+                    <option value="hello">hello</option>
+                </select>
+            </div>
+            <input className="w-full bg-gray-200 text-lg rounded-lg file:border-0 file:bg-gray-300 file:hover:bg-gray-50" type="file" name="" id=""/>
+            <label className="block text-lg">description</label>
+            <textarea
+                className="w-full pl-2 outline outline-1 rounded text-lg"
                 value={itm.description} 
                 onChange={(e)=>handleChange(e,"description")}>
             </textarea>
-            <input type="submit" value="submit" />
+            <div className="flex justify-end mt-2">
+                <input className="mt-2 p-1 rounded bg-cyan-700 hover:bg-cyan-300 text-white"
+                    type="submit" value="submit" />
+            </div>
         </form>
     )
 }
